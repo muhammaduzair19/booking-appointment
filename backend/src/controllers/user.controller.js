83712076ea5a02dddbd85f570cd21d9989b81705
muhammaduzair19 <muhammaduzairilyas@gmail.com>
2024-10-28 20:32:22 +0500

    booking page updates

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { User } from "../models/userSchema.js";
 import { uploadOnCloudinary } from "../config/cloudinary.js";
+import { Doctor } from "../models/doctorSchema.js";
+import { Appointment } from "../models/appointmentModel.js";
 // api register user
 
 export const registerUser = async (req, res) => {
@@ -95,7 +97,7 @@ export const loginUser = async (req, res) => {
 export const getUserData = async (req, res) => {
     try {
         const { userId } = req.body;
-        const user = await User.findById(userId);
+        const user = await User.findById(userId).select("-password");
         res.json({ success: true, user });
     } catch (error) {
         console.log(error);
@@ -128,6 +130,76 @@ export const updateProfile = async (req, res) => {
             await User.findByIdAndUpdate(userId, { image: imageURL });
         }
         res.json({ success: true, user, message: "Profile Updated" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// book appointment
+
+export const bookAppointment = async (req, res) => {
+    try {
+        const { userId, docId, slotTime, slotDate } = req.body;
+        const docData = await Doctor.findById(docId).select("-password");
+
+        if (!docData.available) {
+            return res.json({
+                success: false,
+                message: "Doctor not available",
+            });
+        }
+
+        let slots_booked = docData.slots_booked;
+        if (slots_booked[slotDate]) {
+            if (slots_booked[slotDate].includes(slotTime)) {
+                return res.json({
+                    success: false,
+                    message: "Slot not available",
+                });
+            } else {
+                slots_booked[slotDate].push(slotTime);
+            }
+        } else {
+            slots_booked[slotDate] = [];
+            slots_booked[slotDate].push(slotTime);
+        }
+
+        const userData = await User.findById(userId).select("-password");
+
+        delete userData.slots_booked;
+
+        const appointmentData = {
+            userId,
+            docId,
+            userData,
+            docData,
+            amount: docData.fees,
+            slotTime,
+            slotDate,
+            date: Date.now(),
+        };
+
+        const newAppointment = new Appointment(appointmentData);
+        await newAppointment.save();
+
+        //save new slots data in doc data
+        await Doctor.findByIdAndUpdate(docId, { slots_booked });
+        res.json({ success: true, message: "Appointment Booked" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+};
+
+// api to get all appointment that we booked
+
+export const listAppointment = async (req, res) => {
+    try {
+        const { userId } = req.body;
+
+        const appointments = await Appointment.find({ userId });
+        res.json({ success: true, appointments });
     } catch (error) {
         console.log(error);
         res.json({ success: false, message: error.message });
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,23 +1,32 @@
 import express from "express";
 import {
+    bookAppointment,
     getUserData,
+    listAppointment,
     loginUser,
     registerUser,
     updateProfile,
 } from "../controllers/user.controller.js";
 import authUser from "../middlewares/authUser.js";
+
 import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
+
 userRouter.post("/login", loginUser);
+
 userRouter.get("/get-profile", authUser, getUserData);
+
 userRouter.post(
     "/update-profile",
     upload.single("image"),
     authUser,
     updateProfile
 );
+userRouter.post("/book-appointment", authUser, bookAppointment);
+
+userRouter.get("/appointments", authUser, listAppointment);
 
 export default userRouter;
